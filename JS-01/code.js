e06581ea6b776e2d97b583f64ec042ee27a679b9
Juhee <juhee.jaiswal@gmail.com>
2025-07-04 10:36:22 +0530

    Changes

---
+++ JS-01/code.js
@@ -0,0 +1,9 @@
+onEvent("button1", "click", function( ) {
+  setText("label1", "Gold Facial 1000/- Diamond facial- 2000/-, pearl Facial- 3000/-");
+});
+onEvent("button2", "click", function( ) {
+  setText("label2", "Step Cut 1000/-, Layer cut-1000/- straight cut-500/-");
+});
+onEvent("button3", "click", function( ) {
+  setText("label7", "hand-300/- Leg-400/- Full-1000/-");
+});

21b0aea0829e69d7972035c552d68b530fb4a791
Juhee <juhee.jaiswal@gmail.com>
2025-07-04 11:25:24 +0530

    JS01-02

---
+++ JS-01-02/code.js
@@ -0,0 +1,12 @@
+onEvent("button1", "click", function( ) {
+  setText("label1", "Kurta payjama,casual wear,shirt, Pant,trousers");
+});
+onEvent("button2", "click", function( ) {
+  setText("label1", "salwar suit, straight pants, office wear,saree");
+});
+onEvent("button3", "click", function( ) {
+  setText("label1", "5-10 years,11-15 years");
+});
+onEvent("button4", "click", function( ) {
+  setText("label1", "0-6 months,9-12 months,1-2 years,2-5 years");
+});
